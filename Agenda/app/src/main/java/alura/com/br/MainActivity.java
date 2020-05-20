988a668a0d22f32a5df002e6fe2de57ef0f6eb17
Denis Silva Oliveira <denis_s_oliveira@whirlpool.com>
2020-05-20 20:36:49 -0300

    Implementando o layout da Activity: Create app layout using xml layout resource file

--- Agenda/app/src/main/java/alura/com/br/MainActivity.java
+++ Agenda/app/src/main/java/alura/com/br/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Dênis Silva Oliveira");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
 }
