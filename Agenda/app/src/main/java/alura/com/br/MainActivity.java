07db3c15c758fa33afb012783463144fcbcade5f
Denis Silva Oliveira <denis_s_oliveira@whirlpool.com>
2020-05-23 01:38:43 -0300

    (Curso 1) Android parte 1: Crie a sua primeira app mobile - (Aula 04) Trabalhando com arquivo fonte do layout - (Seção 03) Inserindo ícone e App Bar: Insert App Bar and update its title and add comments to java code

--- Agenda/app/src/main/java/alura/com/br/MainActivity.java
+++ Agenda/app/src/main/java/alura/com/br/MainActivity.java
@@ -1,24 +1,32 @@
 package alura.com.br;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+// Using AppCompatActivity to load App Bar in the app and give support to older versions of Android
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // Run super class onCreate() method (it is mandatory to do that)
         super.onCreate(savedInstanceState);
+        // Load the layout
         setContentView(R.layout.activity_main);
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
+        // Change the title shown on the app bar
+        setTitle("Lista de Alunos");
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
+        // Get view of the List View created in the layout using its ID
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        // Load the list of students in the ListView
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
