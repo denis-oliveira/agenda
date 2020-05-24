a843726f27845a6b69ba997b6e1c702b27d474dd
Denis Silva Oliveira <denis_s_oliveira@whirlpool.com>
2020-05-23 21:57:17 -0300

    (Curso 1) Android parte 1: Crie a sua primeira app mobile - (Aula 05) Finalizando o fluxo do App - (Seção 01) Criando o aluno: Get and store information from student in new student view. Create class to represent a student

--- Agenda/app/src/main/java/alura/com/br/ui/activity/FormularioAlunoActivity.java
+++ Agenda/app/src/main/java/alura/com/br/ui/activity/FormularioAlunoActivity.java
@@ -1,16 +1,45 @@
 package alura.com.br.ui.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import alura.com.br.R;
+import alura.com.br.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_formulario_aluno);
+
+        // Creates views for the layout elements (button and text boxes)
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+
+        // Creates listener for button clicks (get click events)
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get text from the text boxes
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                // Classe to store student information
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+
+                // Show student information
+                Toast.makeText(FormularioAlunoActivity.this, alunoCriado.getNome() + " - " + alunoCriado.getTelefone() + " - " + alunoCriado.getEmail(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
